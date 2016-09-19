a3393e171b0d8a7ca16524abe4df5c8814fc19d5
Marksdon <412176694@qq.com>
2016-09-19 18:35:33 +0800

    Update SqlConfig.java

--- concurrServer/src/com/xzc/concurrServer/util/SqlConfig.java
+++ concurrServer/src/com/xzc/concurrServer/util/SqlConfig.java
@@ -3,8 +3,8 @@ package com.xzc.concurrServer.util;
 public class SqlConfig {
 	
 	public static final String DBADDR2 = "27.17.18.131:38191/urun_opinion_gz_blog";
-	public static final String USER2 = "yunrun";
-	public static final String PASSWORD2 = "Yunrun2015!@#";
+	public static final String USER2 = "***";
+	public static final String PASSWORD2 = "****";
 	public static final String url = "jdbc:mysql://"+DBADDR2+"?"
 			+ "user="+USER2+"&password="+PASSWORD2+"&useUnicode=true&characterEncoding=UTF8";
 	
